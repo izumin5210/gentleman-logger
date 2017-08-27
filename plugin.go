df56330710705862a9c3650561efb03db600fdec
izumin5210 <masayuki@izumin.info>
2017-08-27 10:13:58 +0900

    Add plugin skeleton

--- plugin.go
+++ plugin.go
@@ -1,1 +1,20 @@
 package gentlemanlogger
+
+import (
+	"io"
+
+	"gopkg.in/h2non/gentleman.v2/context"
+	"gopkg.in/h2non/gentleman.v2/plugin"
+)
+
+// New creates logger plugin instance
+func New(out io.Writer) plugin.Plugin {
+	p := plugin.New()
+	p.SetHandler("request", func(c *context.Context, h context.Handler) {
+		h.Next(c)
+	})
+	p.SetHandler("response", func(c *context.Context, h context.Handler) {
+		h.Next(c)
+	})
+	return p
+}
