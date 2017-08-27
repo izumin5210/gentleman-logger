cc0396ddf0dacfeaab743e383fd4f6c6cd477815
izumin5210 <masayuki@izumin.info>
2017-08-27 10:14:56 +0900

    Make log request and response

--- plugin.go
+++ plugin.go
@@ -3,6 +3,7 @@ package gentlemanlogger
 import (
 	"io"
 
+	"github.com/izumin5210/httplogger"
 	"gopkg.in/h2non/gentleman.v2/context"
 	"gopkg.in/h2non/gentleman.v2/plugin"
 )
@@ -10,10 +11,13 @@ import (
 // New creates logger plugin instance
 func New(out io.Writer) plugin.Plugin {
 	p := plugin.New()
+	logger := httplogger.NewLogger(out)
 	p.SetHandler("request", func(c *context.Context, h context.Handler) {
+		c.Request = logger.LogRequest(c.Request)
 		h.Next(c)
 	})
 	p.SetHandler("response", func(c *context.Context, h context.Handler) {
+		logger.LogResponse(c.Response)
 		h.Next(c)
 	})
 	return p
